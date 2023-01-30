8faf72babcaca8b038fb3b8d2e85551d02cd5f28
ckunakom <ckunakom@gmail.com>
2023-01-29 19:22:14 -0500

    renamed some files; renamed some titles; completed the js for navbar

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,6 +1,40 @@
 // Mixing vanilla w jquery
 var navBarDom = `
-
+          <div class="sidebar-sticky pt-3 text-left">
+            <a class="navbar-brand" href="https://ckunakom.github.io/" target="_blank">
+              <img src="https://ckunakom.github.io/img/logo.png" class="rounded mx-auto d-block" alt="ck-boba" width="55px" height="55px">
+            </a>
+            <ul class="nav flex-column text-left">
+              <li class="nav-item">
+                <a class="nav-link" href="index.html">Backstory</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="overview.html">
+                  Overview
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="collab_playback.html">
+                  Collaboration & Playback
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="buddies.html">
+                  Buddies
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="top.html">
+                  Top Songs
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="bubble.html">
+                  Singing Bubble
+                </a>
+              </li>
+            </ul>
+          </div> 
 `;
 
 document.getElementById("repeat-nav-bar").innerHTML = navBarDom;
@@ -8,8 +42,9 @@ document.getElementById("repeat-nav-bar").innerHTML = navBarDom;
 // put active class on the current page
 $(function () {
   var path = window.location.pathname.split("/").pop();
+  console.log(path);
 
-  $('#navbarColor01 li a[href="' + path + '"]')
+  $('#sidebarMenu li a[href="' + path + '"]')
     .parent()
-    .addClass("active");
+    .addClass("nav_active");
 });
