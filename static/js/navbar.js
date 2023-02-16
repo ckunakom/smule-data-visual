4b2793f73bb4f76050cac322cd18158dc2f96db1
ckunakom <ckunakom@gmail.com>
2023-02-16 18:05:10 -0500

    Renamed a page, updated img and home page

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -14,8 +14,8 @@ var navBarDom = `
                 </a>
               </li>
               <li class="nav-item">
-              <a class="nav-link" href="buddies.html">
-                Buddies
+              <a class="nav-link" href="users_recordings.html">
+                Users & Recordings
               </a>
               </li>
               <li class="nav-item">
